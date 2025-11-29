2c37b031d4e46cb20343a312221955ed8a19df5b
sameerraza7760 <sameerraza7760@gmail.com>
2025-11-29 23:29:56 +0500

    Add new functions for array manipulation and string reversal in app2.js

--- app2.js
+++ app2.js
@@ -64,18 +64,98 @@
 // const check = vowel.filter((elem) => arr.includes(elem));
 // console.log(check);
 
+//
+// const nestedArray = [1, 2, 3, 4, [4, 5, 6], [3, 1, [0, 3]]];
 
+// const flatArray = (arr) => {
+//   if (typeof arr !== "object") return;
 
+//   const flatenArray = [];
+//   let sum = 0;
+//   arr.forEach((element) => {
+//     if (element < 3) return;
+//     if (Array.isArray(element)) {
+//       const response = flatArray(element);
+//       flatenArray.push(...response);
+//       sum += response.reduce((acc, curr) => acc + curr, 0);
+//       return;
+//     } else {
+//       flatenArray.push(element);
+//       sum += element;
+//     }
+//   });
 
+//   console.log(sum);
+//   return flatenArray;
+//   //   return flatenArray.reduce((acc, curr) => acc + curr, 0);
+// };
 
+// console.log(flatArray(nestedArray));
 
+//
 
+// const flatArray = (arr) => {
+//   if (typeof arr !== "object") return;
+//   const flatenArray = [];
 
+//   arr.forEach((element) => {
+//     if (Array.isArray(element)) {
+//       console.log("array", element);
+//       const response = flatArray(element);
+//       flatenArray.push(...response);
+//       return;
+//     } else {
+//       flatenArray.push(element);
+//     }
+//   });
+//   return flatenArray;
+// };
 
+// console.log(
+//   flatArray(nestedArray)
+//     .filter((element) => element > 3)
+//     .reduce((acc, curr) => acc + curr, 0)
+// );
 
+// const reverseString = (str) => {
+//   let reversed = "";
+//   for (let index = str.length - 1; index >= 0; index--) {
+//     reversed += str[index];
+//   }
+//   return reversed;
+// };
+// console.log(reverseString("sameer"));
 
+// const findDuplicate = (arr) => {
+//   const include = [];
+//   const duplicate = [];
+//   arr.forEach((element) => {
+//     if (include.includes(element)) {
+//       duplicate.push(element);
+//       return;
+//     }
+//     include.push(element);
+//   });
+//   return include;
+// };
+// console.log(findDuplicate([1, 2, 3, 4, 1, 4]));
 
+// const findMaximum = (arr) => {
+//   const uniqueArr = [...new Set(arr)];
+//   return uniqueArr.sort((a, b) => b - a)[0];
+// };
+// console.log(findMaximum([1, 2, 3, 4, 55, 5, 1, 4]));
 
-
-
-
+const vowel = ["a", "e", "i", "o", "u"];
+const checkVowel = (arr) => {
+  const length = [];
+  arr.forEach((element) => {
+    if (vowel.includes(element)) {
+        return
+    
+    }
+      length.push(element);
+  });
+  return length;
+};
+console.log(checkVowel(["s", "o", "u"]));
